a8e0f97b0f3d8da1838de7a160290378c46d5e3d
Csonka Valentin <valesz0823@gmail.com>
2024-06-20 03:27:47 +0200

    Added error front-end
    The error page is now pretty.

--- View/error.php
+++ View/error.php
@@ -1,3 +1,23 @@
-<h1><?php echo (isset($code) ? $code : "") . " " . (isset($title) ? $title : "") ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nina - Error</title>
+    <link rel="stylesheet" href="./View/css/navbar.css">
+    <link rel="stylesheet" href="./View/css/global.css">
+    <link rel="stylesheet" href="./View/css/listing.css">
+</head>
+<body>
+    
+    <?php include "Fragments/header.php" ?>
 
-<p><?php echo ($description ? $description : "") ?></p>
+    <main>
+        <h1><?= isset($code) ? $code : "" ?> <?= isset($title) ? $title : "" ?></h1>
+        <p><?= isset($description) ? $description : "" ?></p>
+    </main>
+
+    <?php include "Fragments/footer.php" ?>
+
+</body>
+</html>
