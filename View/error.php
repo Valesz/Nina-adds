4b3e394370bb4dc90c35bf1d52c434f378ee7782
Csonka Valentin <valesz0823@gmail.com>
2024-06-30 20:38:15 +0200

    Error page bug fixed
    When you visited a non-existing subpage it wouldn't load in the css normally.

--- View/error.php
+++ View/error.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nina - Error</title>
-    <link rel="stylesheet" href="./View/css/navbar.css">
-    <link rel="stylesheet" href="./View/css/global.css">
-    <link rel="stylesheet" href="./View/css/listing.css">
+    <link rel="stylesheet" href="/View/css/navbar.css">
+    <link rel="stylesheet" href="/View/css/global.css">
+    <link rel="stylesheet" href="/View/css/listing.css">
 </head>
 <body>
     
